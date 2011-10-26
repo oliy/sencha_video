83cd8405c1ef181e06fbb09c5b7ef71bafd2e7e0
Oli Yu <oli@tippr.com>
2011-10-26 12:52:18 -0500

    Working viewport.

--- public/app.js
+++ public/app.js
@@ -8,23 +8,10 @@ Ext.application({
     ],
 
     launch: function() {
-        Ext.create('Ext.container.Viewport', {
-            layout: 'border',
-            items: [
-                {
-                    xtype: 'panel',
-                    region: 'north',
-                    title: 'Header',
-                    height: 500,
-                    html: 'Header'
-                },
-                {
-                    xtype: 'panel',
-                    region: 'center',
-                    title: 'Content',
-                    html: 'Content'
-                }
-            ]
+        console.log('View!');
+
+        Ext.create('App.view.ViewPort', {
+
         });
     }
 });
